e2454599b57e8c5bf8f49b2b610d14410b5e5ec6
Jeff Chadrick <chadrick.jeff@gmail.com>
2024-12-17 21:55:53 -0500

    7: Add an Area calculator to the Circumference Calc
    Add an area calculator to the perimeter calculator. Use control structures to allow the user to calculate only the area, only the circumference, or both.

---
+++ circ_and_area/Main.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main {
+        //method to calc area
+    public static void Area (double r) {
+        double area = Math.pow(r, 2) * Math.PI;
+        System.out.println("Area: " + area);
+    }
+        //method to calc circumference
+    public static void Circ (double r) {
+        double circ = 2 * Math.PI * r;
+        System.out.println("Circumference: " + circ);
+    }
+
+        // main program begins
+    public static void main (String[] args) {
+            // creates the scanner and the variables needed for calculation selection.
+        Scanner scanner = new Scanner(System.in);
+        String user_choice = "";
+        boolean t = false; // this is used as part of the while validation
+
+            // while loop to test for valid input of area or circumference.
+        while (!t) {
+
+                // block is printing twice for some reason.
+            System.out.println("Please choose: ");
+            System.out.println("A: Area");
+            System.out.println("C: Circumference");
+            System.out.println("B: Both");
+            user_choice = scanner.nextLine();
+
+                // to adjust t to true when a valid selection is made.
+                // probably more clunky than necessary, might circle back to refine.
+            if (user_choice.equalsIgnoreCase("a")
+                    || user_choice.equalsIgnoreCase("c")
+                    || user_choice.equalsIgnoreCase("b")) {
+                t = true;
+            }
+        }
+
+            // gather radius from user
+        System.out.println("Enter a radius: ");
+        double r = scanner.nextDouble();
+
+            // if checks for which calculations to make and calls the appropriate method
+        if (user_choice.equalsIgnoreCase("a")) {
+            Area(r);
+        } else if (user_choice.equalsIgnoreCase("c")) {
+            Circ(r);
+        } else {
+            Area(r);
+            Circ(r);
+        }
+    }
+}
